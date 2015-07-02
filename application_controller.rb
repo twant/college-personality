e0d2474fe1a903cd93eceab86e7ea17d7b03c8db
twant <taylor.want@gmail.com>
2015-07-02 20:06:13 +0000

    it works git push origin master

--- application_controller.rb
+++ application_controller.rb
@@ -13,10 +13,8 @@ class ApplicationController < Sinatra::Base
   end
   
   post '/questionnaire' do
-    binding.pry
     new_arr=[] 
     params.each{|key,value| new_arr.push(value)}
-    new_arr.pop
     @results=college_picker(new_arr)
     erb :results
   end
--- models/college.rb
+++ models/college.rb
@@ -1,3 +1,4 @@
+require 'set' 
 class Array
   def included_in?(array)
     array.to_set.superset?(self.to_set)
